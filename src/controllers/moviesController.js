fe89231860b41c27471a906a0b62c84ca4b88a8e
TaniaRivas1 <taniarivas140@gmail.com>
2022-09-09 02:08:30 -0300

    tp bd manipulacion de datos

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,124 @@
+const db = require('../database/models');
+const sequelize = db.sequelize;
+const moment = require('moment')
+//Otra forma de llamar a los modelos
+const Movies = db.Movie;
+
+const moviesController = {
+    'list': (req, res) => {
+        db.Movie.findAll()
+            .then(movies => {
+                res.render('moviesList.ejs', {movies})
+            })
+    },
+    'detail': (req, res) => {
+        db.Movie.findByPk(req.params.id)
+            .then(movie => {
+                res.render('moviesDetail.ejs', {movie});
+            });
+    },
+    'new': (req, res) => {
+        db.Movie.findAll({
+            order : [
+                ['release_date', 'DESC']
+            ],
+            limit: 5
+        })
+            .then(movies => {
+                res.render('newestMovies', {movies});
+            });
+    },
+    'recomended': (req, res) => {
+        db.Movie.findAll({
+            where: {
+                rating: {[db.Sequelize.Op.gte] : 8}
+            },
+            order: [
+                ['rating', 'DESC']
+            ]
+        })
+            .then(movies => {
+                res.render('recommendedMovies.ejs', {movies});
+            });
+    }, //Aqui debemos modificar y completar lo necesario para trabajar con el CRUD
+    add: function (req, res) {
+    db.Genre.findAll({
+order: [
+    ['name','ASC']
+]
+
+    })
+    .then(genres =>{
+        return res.render('moviesAdd',{genres
+        })
+    })
+    .catch(error=>console.log(error))
+       
+    },
+    create: function (req, res) {
+        const {title,awards,release_date,genre_id,rating,length} = req.body
+      db.Movie.create({
+        title : title.trim(),
+        awards : +awards,
+        release_date,
+        genre_id : +genre_id,
+        rating : +rating,
+        length : +length
+      })
+
+      .then(movie => {
+        console.log(movie);
+        return res.redirect('/movies/detail/' + movie.id)
+      })
+      .catch(error => console.log(error))
+    },
+    edit: function(req, res) {
+       let movie =  db.Movie.findByPk(req.params.id)
+       let genres = db.Genre.findAll({
+        order : ['name']
+       })
+       Promise.all([movie,genres])
+        .then(([movie,genres]) =>{
+            return res.render('moviesEdit', {
+                Movie:movie,
+                release_date: moment(movie.release_date).format('YYYY-MM-DD'),
+                genres
+            })
+        })
+        .catch(error =>console.log(error))
+    },
+    update: function (req,res) {
+        const {title,awards,release_date,genre_id,rating,length} = req.body
+        //return res.send(req.body)
+        db.Movie.update({
+            title : title.trim(),
+            awards : +awards,
+            release_date,
+            genre_id : +genre_id,
+            rating : +rating,
+            length : +length
+        },{
+            where:{
+                id: req.params.id
+            }
+        }).then(() => res.redirect('/movies'))
+        .catch(error => console.log(error))
+    },
+    delete: function (req, res) {
+        db.Movie.findByPk(req.params.id)
+        .then(movie => res.render('moviesDelete',{
+            movie
+        })).catch(error => console.log(error))
+    },
+    destroy: function (req, res) {
+        //return res.send(req.params.id)
+        db.Movie.destroy({
+            where:{
+                id: req.params.id
+            }
+        }).then(()=> res.redirect('/movies'))
+        .catch(error => console.log(error))
+    }
+}
+
+module.exports = moviesController;
